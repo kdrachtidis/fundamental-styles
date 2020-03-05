768724c3b48637aabbba90bf88ad609fddce2c78
Konstantinos Drachtidis <konstantinos.drachtidis@sap.com>
2020-03-05 18:37:03 +0100

    Regions fix

--- sources/script/globalGetData.js
+++ sources/script/globalGetData.js
@@ -51,6 +51,8 @@ function selectAccount(currentId) {
 function selectTenant(currentId) {
     globalSelectedTenant = currentId.substring(currentId.length - 2, currentId.length) - 1;
     globalSelectedAccount = 0;
+    globalSelectedRegion = currentId.substring(7, 8) - 1;
+    console.log(globalSelectedRegion);
     $('#leftSidebar .fd-list__item').attr('aria-selected','false');
     $.when(getTenantData()).done(function(){
         $('#' + currentId).attr('aria-selected','true');
