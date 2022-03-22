c6152a724a90b1cf4bd55767af95e885dbfe7b41
Konstantinos Drachtidis <konstantinos.drachtidis@sap.com>
2022-03-22 14:47:38 +0100

    sectionColumns level eliminated

--- sources/script/globalLoad.js
+++ sources/script/globalLoad.js
@@ -1,45 +1,27 @@
 function loadPrototypeIndex() {
-    console.log('loadPrototypeIndex()');
-
-    $.when(loadShell()).done(getInitialData());
-    
+    loadShell();
+    $.when(loadPanels()).done(function(){
+        console.log('loadPrototypeIndex()');
+        getInitialData();
+    });
     return true;
 }
 
-function loadPrototypePage(){
-    console.log('loadPrototypePage()');
+function loadPrototypeSection() {
 
-    $.when(loadSection()).done(getInitialData());
-
-    return true;
-}
-
-function loadPrototypeSection(){
-    console.log('loadPrototypeSection()');
-
-    $.when(loadPanels()).done(getInitialData());
+    $.when(loadPanels()).done(function () {
+        console.log('loadPrototypeSection()');
+        getInitialData();
+    });
 
     return true;
 }
 
 function loadShell() {
     console.log('loadShell()');
-    
+
     $('.fd-shell__header').load(globalSourcePath + 'src/html/shellBar.min.html .fd-shellbar');
-    $('.fd-app__main').load(globalSourcePath + 'src/html/pageContainer.min.html .fd-page', function () {
-        loadSection();
-    });
     $('.fd-shell__footer').load(globalSourcePath + 'src/html/footerBar.min.html .fd-bar');
-
-    return true;
-}
-
-function loadSection() {
-    console.log('loadSection()');
-
-    $('.fd-page__content').load(globalSourcePath + 'src/html/sectionColumns.min.html .fd-section', function () {
-        loadPanels();
-    });
 
     return true;
 }
@@ -52,6 +34,6 @@ function loadPanels() {
     $('#mainContainer-shell').load(globalSourcePath + 'src/html/mainContainer.min.html .fd-panel');
     $('#rightContainer-shell').load(globalSourcePath + 'src/html/rightContainer.min.html .fd-panel');
     $('#rightSidebar-shell').load(globalSourcePath + 'src/html/rightSidebar.min.html .fd-panel');
-    
+
     return true;
 }
